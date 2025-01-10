53eb1ebe539408ff03fe0b25b88ada43bac9d7bd
Marco Maier <marco.maier@cloudhero.it>
2025-01-10 11:45:56 +0100

    add package Fluent UI

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 using Portfolio;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddFluentUIComponents();
+builder.Services.AddHttpClient();
+builder.Services.AddFluentUIComponents(options =>
+{
+    options.ValidateClassNames = false;
+});
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
