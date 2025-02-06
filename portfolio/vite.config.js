b268fd04d0025debc84959d84bb17089fa1db2e2
Marco Maier <marco.maier@cloudhero.it>
2025-02-06 17:32:18 +0100

    init utilizzo tailwind

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        tailwindcss(),
+        react()
+    ],
     server: {
         host: '0.0.0.0',  // Permette connessioni da qualsiasi IP
         port: 5173,       // Puoi cambiare la porta se vuoi
