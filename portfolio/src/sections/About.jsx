a9472b42d29e94a1ba3e0c511ad3824018b79afe
marcomaier <marcomaier3903@gmail.com>
2025-02-17 19:32:50 +0100

    aggiunta card con globo

--- portfolio/src/sections/About.jsx
+++ portfolio/src/sections/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Globe from 'react-globe.gl'
 
 const About = () => {
   return (
@@ -18,6 +19,27 @@ const About = () => {
             <div className='col-span-1 xl:row-span-3'>
                 <div className='grid-container'>
                     <img src='/assets/grid2.png' alt='grid-2' className='w-full sm:h-[276px] h-fit object-contain' />
+                    <div>
+                        <p className='grid-headtext'>Tech Stack</p>
+                        <p className='grid-subtext'>I specialize in .NET, SQL, HTML, CSS, Javascript.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='col-span-1 xl:row-span-4'>
+                <div className='grid-container'>
+                    <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
+                        <Globe
+                        height={326}
+                        width={326}
+                        backgroundColor='rgba(0,0,0,0)'
+                        backgroundImageOpacity={0.5}
+                        showAtmosphere
+                        showGraticules
+                        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                        //labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+                         />
+                    </div>
                 </div>
             </div>
         </div>
