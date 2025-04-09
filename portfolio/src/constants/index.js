90c104f3183a80e9438e7fa5faf0dafa9f2de348
marcomaier <marcomaier3903@gmail.com>
2025-04-09 19:51:20 +0200

    experience

--- portfolio/src/constants/index.js
+++ portfolio/src/constants/index.js
@@ -260,29 +260,46 @@ export const navLinks = [
   export const workExperiences = [
     {
       id: 1,
-      name: 'Framer',
-      pos: 'Lead Web Developer',
-      duration: '2022 - Present',
-      title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
-      icon: 'assets/framer.svg',
+      name: 'Cloudhero',
+      pos: 'Full-stack Developer',
+      duration: '2023 - Present',
+      title: "I develop custom projects for third-party clients from scratch, " +
+              "using a range of technologies including C#, HTML, " + 
+              "CSS, Javascript, SQL, NoSQL, Docker, and Kubernetes. " +
+              "I actively participate in the startup phase of new projects, " +
+              "conducting data analysis to determine the optimal data structure " +
+              ". I collaborate with cross-functional teams to " +
+              "ensure that technology solutions are aligned with " +
+              "business objectives and meet customer needs. " +
+              "Translated with DeepL.com (free version)",
+      icon: 'assets/logoCH.jpeg',
       animation: 'victory',
     },
     {
       id: 2,
-      name: 'Figma',
-      pos: 'Web Developer',
-      duration: '2020 - 2022',
-      title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
-      icon: 'assets/figma.svg',
+      name: 'Microsoft Cloud Developer Academy',
+      pos: 'Cloud Developer',
+      duration: '2022 - 2023',
+      title: "This course was mainly based " +
+              "on Asp.Net development. Then we covered the " +
+              "c# language, databases (SQL Server, SQLite, " +
+              "CosmosDB), and the azure world (Azure Portal, Azure " +
+              "DevOps) and then publishing WebApps in the cloud.",
+      icon: 'assets/MicrosoftLogo.png',
       animation: 'clapping',
     },
     {
       id: 3,
-      name: 'Notion',
-      pos: 'Junior Web Developer',
-      duration: '2019 - 2020',
-      title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
-      icon: 'assets/notion.svg',
-      animation: 'salute',
+      name: 'Talent Garden - Cyber Security Bootcamp',
+      pos: 'Cyber Security Analyst',
+      duration: '2021 - 2022',
+      title: "This course was based on key concepts to learn about linux " +
+              "and OS, learn about digital forensics processes for analyzing " +
+              "threats in digital devices, perform " +
+              "cyber attacks, cyber defense practices and " +
+              "vulnerability assessment, and finally gain " +
+              "familiarity with the latest trends and technologies.",
+      icon: 'assets/talentGardenLogo.jpeg',
+      animation: 'clapping',
     },
   ];
