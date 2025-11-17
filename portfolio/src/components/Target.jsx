51811a7772cfee276c57bf3c09c70ed4a3db8887
marcomaier <marcomaier3903@gmail.com>
2025-11-17 21:28:37 +0100

    fix models target

--- portfolio/src/components/Target.jsx
+++ portfolio/src/components/Target.jsx
@@ -1,26 +1,40 @@
-import { useGLTF } from '@react-three/drei';
-import React, { useRef } from 'react';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
+import { Float, useGLTF, Environment } from '@react-three/drei';
 
 const Target = (props) => {
-    const targetRef = useRef();
-    const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf');
+  const base = import.meta.env.BASE_URL;
+  const { nodes } = useGLTF(`${base}models/windowsLogo.glb`);
 
-    useGSAP(()=>{
-        gsap.to(targetRef.current.position, {
-            y: targetRef.current.position.y + 0.5,
-            duration: 1.5,
-            repeat: -1,
-            yoyo: true,
-        })
-    });
+  return (
+    <>
+      {/* Riflessi reali */}
+      <Environment preset="city" />
 
-    return ( 
-        <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
-            <primitive object={scene} />
-        </mesh>
-    );
-}
- 
+      <Float floatIntensity={1}>
+        <group
+          position={[0, 0, 0]}
+          scale={[0.1, 0.1, 0.1]}
+          rotation={[3, 1.3, 1]}
+          {...props}
+          dispose={null}
+        >
+          <mesh geometry={nodes.Windows_8_0.geometry}>
+            <meshPhysicalMaterial
+              color="#00A4EF"
+              roughness={0.1}        // superficie liscia
+              metalness={0.2}        // leggero effetto metal
+              clearcoat={1}          // effetto vetro
+              clearcoatRoughness={0} // clearcoat lucido
+              envMapIntensity={1.5}  // forza del riflesso
+              transmission={0.05}    // leggero effetto "vetro"
+              thickness={0.1}
+            />
+          </mesh>
+        </group>
+      </Float>
+    </>
+  );
+};
+
+useGLTF.preload(`${import.meta.env.BASE_URL}models/windowsLogo.glb`);
+
 export default Target;
